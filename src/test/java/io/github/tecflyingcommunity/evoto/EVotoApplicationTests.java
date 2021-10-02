5723bd54a5f6a0a4dcfc619f44e887e81427b261
Pedro Marinho <pedro.marinho238@gmail.com>
2021-10-02 18:52:05 -0400

    Criar a base java
    Criação do projeto e configuração
    
    Tarefa: TECFlyingCommunity/e-vote#1
    
    Signed-off-by: Pedro Marinho <pedro.marinho238@gmail.com>

---
+++ src/test/java/io/github/tecflyingcommunity/evoto/EVotoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tecflyingcommunity.evoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
