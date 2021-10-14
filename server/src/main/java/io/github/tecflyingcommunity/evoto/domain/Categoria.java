78acc4b69a5599909e55a7e2d58859da5b1494c2
Pedro Marinho <pedro.marinho238@gmail.com>
2021-10-14 02:13:57 -0400

    Novas entidades.
    Novas entidade necessárias para o projeto
    
    Tarefa: TECFlyingCommunity/e-vote#8
    
    Signed-off-by: Pedro Marinho <pedro.marinho238@gmail.com>

---
+++ server/src/main/java/io/github/tecflyingcommunity/evoto/domain/Categoria.java
@@ -0,0 +1,99 @@
+package io.github.tecflyingcommunity.evoto.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria   implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Integer id;
+	private String nome;
+	
+	
+	@OneToMany(mappedBy="categoria", cascade=CascadeType.ALL)
+	private List<Candidato> candidatos = new ArrayList<>();
+
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+	public Categoria() {
+		super();
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public List<Candidato> getCandidatos() {
+		return candidatos;
+	}
+
+
+	public void setCandidatos(List<Candidato> candidatos) {
+		this.candidatos = candidatos;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + ", candidatos=" + candidatos + "]";
+	}
+	
+	
+	
+
+}
